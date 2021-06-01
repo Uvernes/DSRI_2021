5f84dbd3a63b60f8bd848ae06d72c3e927b56aa8
Uvernes <56709871+Uvernes@users.noreply.github.com>
2021-06-01 18:46:46 -0400

    Update nearest_rotation_matrix.py

--- nearest_rotation_matrix.py
+++ nearest_rotation_matrix.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import sys
 
 """
 Input
@@ -19,7 +20,7 @@ def exact_nearest_rotation_matrix(R):
     m = np.trace(A) / 3
     Q = A - m * np.identity(3)
     q = np.linalg.det(Q) / 2
-    p = np.sum(tMatrix * 2) / 6
+    p = np.sum(np.power(Q, 2)) / 6
     sp = math.sqrt(p)
 
     theta = math.atan2(math.sqrt(abs(p**3 - q**2)), q) / 3
@@ -37,8 +38,8 @@ def exact_nearest_rotation_matrix(R):
 
     dem = a0*(a2*a1-a0)
 
-    b0 = (a2*a1**2 -a0*(a2**2 +a1))/dem
-    b1 = (a0+a2*(a2**2 - 2*a1))/dem
+    b0 = (a2*(a1**2) - a0 * ((a2**2) + a1))/dem
+    b1 = (a0+a2*((a2**2) - 2*a1))/dem
     b2 = a2/dem
 
     U = b2*A*A -b1*A +b0*np.eye(3)
@@ -50,8 +51,20 @@ def exact_nearest_rotation_matrix(R):
 # tMatrix = np.array([[0.1959, -0.3765, -0.7937],
 #                     [0.7458, 0.5011, -0.0536],
 #                     [0.4643, -0.6459, 0.4210]])
-
+#
 # closest = exact_nearest_rotation_matrix(tMatrix)
+# print("Matrix: ")
+# print(tMatrix)
+# print("Nearest rotation matrix: ")
 # print(closest)
 # # Matrix multiplication below is eye(3), as expected
-# print(np.dot(closest, closest.transpose()))
+# print("Matrix multiplication:")
+# np.savetxt(sys.stdout, np.dot(closest, closest.transpose()), '%.5f')
+#
+# print("---------------")
+# matrix = np.array([[1, 2, 3], [4, 5, 6]])
+# print(matrix)
+# print(np.sum(matrix))
+# print(np.power(matrix, 2))
+# print(np.sum(np.power(matrix, 2)))
+
